8806f5b520bace01abef4d759503129aca875ce0
Frederik Rabøl <frederik-rm@hotmail.com>
2020-01-05 17:22:27 +0100

    mesh created still have a bug on the width. Will look into it

--- Assets/Blob/Scripts/MapGenerator.cs
+++ Assets/Blob/Scripts/MapGenerator.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
+	public enum DrawMode {
+		noiseMap,
+		ColourMap,
+		Mesh
+	}
 
+	public DrawMode drawMode;
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -17,12 +23,32 @@ public class MapGenerator : MonoBehaviour {
 
 	public bool autoUpdate;
 
+	public TerrainType[] regions;
 	public void GenerateMap() {
 		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap[x,y];
+				for(int i = 0; i < regions.Length; i++){
+					if(currentHeight <= regions[i].height){
+						colourMap[y * mapWidth + x] = regions[i].colour;
+						break;
+					}
+				}
+			}
+		}
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		display.DrawNoiseMap (noiseMap);
+		if(drawMode == DrawMode.noiseMap){
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		}
+		else if(drawMode == DrawMode.ColourMap){
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		} else if (drawMode == DrawMode.Mesh){
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
 	}
 
 	void OnValidate() {
@@ -40,4 +66,10 @@ public class MapGenerator : MonoBehaviour {
 		}
 	}
 
+}
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color colour;
 }
