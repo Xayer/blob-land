5e9fb760c5c99d9e9d508d9320be3df699c96f59
Frederik Rabøl <frederik-rm@hotmail.com>
2020-01-05 15:45:41 +0100

    added map generator

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI(){
+        MapGenerator MapGen = (MapGenerator)target;
+        if(DrawDefaultInspector()){
+            if(MapGen.autoUpdate){
+                MapGen.GenerateMap();
+            }
+        }
+
+        if(GUILayout.Button("Generate")){
+            MapGen.GenerateMap();
+        }
+    }
+}
