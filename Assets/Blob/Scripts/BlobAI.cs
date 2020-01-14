4a66b1a7102ed85416e7d063befea8a615f34fe6
Frederik Rabøl <frederik-rm@hotmail.com>
2020-01-14 20:56:42 +0100

    breeding is now possible if you hit the AI blob!

--- Assets/Blob/Scripts/BlobAI.cs
+++ Assets/Blob/Scripts/BlobAI.cs
@@ -7,19 +7,22 @@ public class BlobAI : MonoBehaviour
     public Transform target;
     private UnityEngine.AI.NavMeshAgent agent;
     public GameObject head;
+    public GameObject child;
     public float minRotationX = 13f;
     public float maxRotationX = -47f;
     private float distanceToTarget;
+    private GameObject body;
     public float rotationSmoothing = 20000f;
     
     void Awake(){
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>(); 
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        GameObject.FindWithTag("Player").transform.LookAt(target);
         agent.destination = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
         calculateDistance();
         if(agent.stoppingDistance <= distanceToTarget) {
---
+++ Assets/Blob/Scripts/BreedBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreedBehavior : MonoBehaviour
+{
+    public GameObject child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Player"){
+            this.breedWithPlayer();
+        }
+    }
+
+    void breedWithPlayer(){
+        Vector3 position = transform.position;
+        position.x += 1;
+        position.y += 10;
+        Instantiate(
+                child,
+                position,
+                Quaternion.Euler(0f,0f,0f));
+    }
+}
